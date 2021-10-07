d93758aca609d45927e5e9bcbcfab9cd2cff1518
Ali Madooei <alimadooei@gmail.com>
2021-10-07 10:48:18 -0400

    Step 24: App's state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,78 @@
+import React, { Component } from "react";
 import Definitions from "./components/Definitions";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Phonetics from "./components/Phonetics";
 import Search from "./components/Search";
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Search />
-      <Phonetics />
-      <Definitions />
-      <Footer />
-    </>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phonetics: [
+        {
+          text: "/həˈloʊ/",
+          audio: "https://lex-audio.useremarkable.com/mp3/hello_us_1_rr.mp3",
+        },
+        {
+          text: "/hɛˈloʊ/",
+          audio: "https://lex-audio.useremarkable.com/mp3/hello_us_2_rr.mp3",
+        },
+      ],
+      meanings: [
+        {
+          partOfSpeech: "exclamation",
+          definitions: [
+            {
+              definition:
+                "Used as a greeting or to begin a phone conversation.",
+              example: "hello there, Katie!",
+            },
+          ],
+        },
+        {
+          partOfSpeech: "noun",
+          definitions: [
+            {
+              definition: "An utterance of “hello”; a greeting.",
+              example: "she was getting polite nods and hellos from people",
+              synonyms: [
+                "greeting",
+                "welcome",
+                "salutation",
+                "saluting",
+                "hailing",
+                "address",
+                "hello",
+                "hallo",
+              ],
+            },
+          ],
+        },
+        {
+          partOfSpeech: "intransitive verb",
+          definitions: [
+            {
+              definition: "Say or shout “hello”; greet someone.",
+              example: "I pressed the phone button and helloed",
+            },
+          ],
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <Header />
+        <Search />
+        <Phonetics />
+        <Definitions />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default App;
