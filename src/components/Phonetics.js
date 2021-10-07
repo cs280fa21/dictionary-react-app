9ac73ba95ce26cde5dfa2d9fa46ede3f307df7a3
Ali Madooei <alimadooei@gmail.com>
2021-10-06 23:07:26 -0400

    Step 13: JSX

--- src/components/Phonetics.js
+++ src/components/Phonetics.js
@@ -1,35 +1,37 @@
 function Phonetics() {
-  return (
-    <>
-      <section class="section is-medium pt-0 pb-6" id="phonetics">
-        <h1 class="title">Phonetics</h1>
-        <article class="message is-medium">
-          <div class="message-header">/həˈloʊ/</div>
-          <div class="message-body">
-            <audio controls style={{width: "100%"}}>
-              <source
-                src="https://lex-audio.useremarkable.com/mp3/hello_us_1_rr.mp3"
-                type="audio/mpeg"
-              />
-              Your browser does not support the audio element.
-            </audio>
-          </div>
-        </article>
 
-        <article class="message is-medium">
-          <div class="message-header">/hɛˈloʊ/</div>
-          <div class="message-body">
-            <audio controls style={{width: "100%"}}>
-              <source
-                src="https://lex-audio.useremarkable.com/mp3/hello_us_2_rr.mp3"
-                type="audio/mpeg"
-              />
-              Your browser does not support the audio element.
-            </audio>
-          </div>
-        </article>
-      </section>
-    </>
+  const phonetics = [
+    {
+      text: "/həˈloʊ/",
+      audio: "https://lex-audio.useremarkable.com/mp3/hello_us_1_rr.mp3",
+    },
+    {
+      text: "/hɛˈloʊ/",
+      audio: "https://lex-audio.useremarkable.com/mp3/hello_us_2_rr.mp3",
+    }
+  ];
+
+  return (
+    <section className="section is-medium pt-0 pb-6" id="phonetics">
+      <h1 className="title">Phonetics</h1>
+      {
+        phonetics.map((phonetic, index) => {
+          return (
+            <article className="message is-medium">
+              <div className="message-header">
+                {phonetic.text}
+              </div>
+              <div className="message-body">
+                <audio controls style={{width: "100%"}}>
+                  <source src={phonetic.audio} type="audio/mpeg"/>
+                  Your browser does not support the audio element.
+                </audio>
+              </div>
+            </article>
+          )
+        })
+      }
+    </section>
   );
 }
 
