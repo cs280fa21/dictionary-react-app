958496b13bcc033083650843b38dfcd9f92124a1
Ali Madooei <alimadooei@gmail.com>
2021-10-06 21:04:24 -0400

    Step 10: Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 function Header() {
-  return <div>Header!</div>;
+  return (
+    <header className="hero is-info">
+      <div className="hero-body">
+        <p className="title has-text-centered">
+          Free English Dictionary
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
