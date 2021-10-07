4ed8915f04d8629c387232062901294f3911a48c
Ali Madooei <alimadooei@gmail.com>
2021-10-06 23:22:51 -0400

    Step 14: Definitions component

--- src/components/Definitions.js
+++ src/components/Definitions.js
@@ -1,5 +1,69 @@
 function Definitions() {
-  return <div>Definitions!</div>;
+  const meanings = [
+    {
+      partOfSpeech: "exclamation",
+      definitions: [
+        {
+          definition: "Used as a greeting or to begin a phone conversation.",
+          example: "hello there, Katie!",
+        },
+      ],
+    },
+    {
+      partOfSpeech: "noun",
+      definitions: [
+        {
+          definition: "An utterance of “hello”; a greeting.",
+          example: "she was getting polite nods and hellos from people",
+          synonyms: [
+            "greeting",
+            "welcome",
+            "salutation",
+            "saluting",
+            "hailing",
+            "address",
+            "hello",
+            "hallo",
+          ],
+        },
+      ],
+    },
+    {
+      partOfSpeech: "intransitive verb",
+      definitions: [
+        {
+          definition: "Say or shout “hello”; greet someone.",
+          example: "I pressed the phone button and helloed",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <section className="section is-medium pt-0 pb-6" id="definitions">
+      <h1 className="title">Definitions</h1>
+      {
+        meanings.map((meaning, index) => {
+          return (
+            <article className="message is-info is-medium" key={index}>
+              <div className="message-header">
+                <p>{meaning.partOfSpeech}</p>
+              </div>
+              <div className="message-body">
+                <div className="content">
+                  <ul style={{marginTop: 0}}>
+                    {
+                      meaning.definitions.map((item, i) => <li key={i}>{item.definition}</li>)
+                    }
+                  </ul>
+                </div>
+              </div>
+            </article>
+          );
+        })
+      }
+    </section>
+  );
 }
 
 export default Definitions;
